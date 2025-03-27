d8c8f54a9e71c5d70ab9e657edb168bbd1b13d7e
Muhammad Ibrar <ibrarsh001@gmail.com>
2025-03-27 14:20:38 +0500

    1st commit

---
+++ app/form/page.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { Widget } from "@typeform/embed-react";
+import { useSearchParams } from "next/navigation";
+
+export default function Page() {
+  const searchParams = useSearchParams();
+  const widgetContainerStyle = {
+    width: "80vw",
+    height: 700,
+    margin: "20px auto",
+  };
+
+  return (
+    <Widget
+      id={"dHWxc3pF"}
+      style={widgetContainerStyle}
+      medium="demo-test"
+      hidden={{ foo: "foo value", bar: "bar value" }}
+      transitiveSearchParams={["foo", "bar"]}
+      iframeProps={{ title: "Foo Bar" }}
+    />
+  );
+}
